767aab6b2adeb47e3a31a3180042ed91d099ef1b
tumo <tumo17th@gmail.com>
2015-12-16 07:38:49 +0900

    Modify a little.

---
+++ acbook-core/src/main/java/com/acbook/model/Expense.java
@@ -0,0 +1,16 @@
+package com.acbook.model;
+
+import java.util.Date;
+
+/**
+ * 出費モデル
+ * @author shibata_ts
+ */
+public class Expense {
+
+	private Integer id;
+	private String name;
+	private Integer price;
+	private Date date;
+
+}
