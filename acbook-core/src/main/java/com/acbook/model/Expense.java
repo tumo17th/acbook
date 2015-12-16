37cba9c7919842788d30203766f15ec04eba3dc5
tumo <tumo17th@gmail.com>
2015-12-16 23:49:46 +0900

    Modify a little
    1.Modify classpath
    2.Create Getter & Setter to Expense Model

--- acbook-core/src/main/java/com/acbook/model/Expense.java
+++ acbook-core/src/main/java/com/acbook/model/Expense.java
@@ -13,4 +13,29 @@ public class Expense {
 	private Integer price;
 	private Date date;
 
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
 }
