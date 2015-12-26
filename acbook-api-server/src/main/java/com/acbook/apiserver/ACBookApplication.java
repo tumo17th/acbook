665e09abaee9fba2b1a8a2eb0df42c969c54a308
tumo <tumo17th@gmail.com>
2015-12-27 04:06:55 +0900

    Modify comment

--- acbook-api-server/src/main/java/com/acbook/apiserver/ACBookApplication.java
+++ acbook-api-server/src/main/java/com/acbook/apiserver/ACBookApplication.java
@@ -9,11 +9,18 @@ import com.acbook.apiserver.common.GsonMessageProvider;
 
 public class ACBookApplication extends ResourceConfig {
 
+    /** Rest target resources package */
     private static final String[] RESOURCE_PACKAGES = { "com.acbook.apiserver.resources" };
 
+    /**
+     * Constructor >> Initialize process
+     * 
+     * 1.Set LoggingFilter
+     * 2.Set response charset to UTF-8
+     * 3.Set Gson serializer to response entity
+     */
     public ACBookApplication() {
         packages(RESOURCE_PACKAGES);
-        // register(ACBookContextLoaderListener.class);
         register(LoggingFilter.class);
         register(CharsetResponseFilter.class);
         register(GsonMessageProvider.class);
